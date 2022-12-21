86e5798fc99c5ee832506d67e0ebe26e684482cc
stefanbalta <stefan.balta.sb@gmail.com>
2022-12-20 21:40:08 -0500

    Added day1 code and debugging configuration in lauch.json

---
+++ src/day1.rs
@@ -0,0 +1,41 @@
+use std::{fs::File, io::Read, path::Path};
+
+pub fn calorie_counter() -> i64 {
+    let path = Path::new("day1.txt");
+    let mut file = match File::open(&path) {
+        Err(_) => panic!("could not open file!"),
+        Ok(file) => file,
+    };
+    let mut raw_calories = String::new();
+    match file.read_to_string(&mut raw_calories) {
+        Err(e) => panic!("Error reading from file {}", { e }),
+        Ok(lenght) => lenght,
+    };
+    let calories: Vec<i64> = raw_calories
+        .split("\n")
+        .map(|x| x.parse::<i64>().unwrap_or_default())
+        .collect();
+    dbg!(&calories);
+    let mut highest_elves: Vec<i64> = vec![0, 0, 0];
+    let mut current_elf: i64 = 0;
+    for calorie in calories {
+        if calorie != 0 {
+            current_elf += calorie;
+        } else {
+            for elf in &highest_elves {
+                if &current_elf > elf {
+                    highest_elves.remove(0);
+                    highest_elves.push(current_elf.clone());
+                    highest_elves.sort();
+                    break;
+                }
+            }
+            current_elf = 0;
+        }
+    }
+    current_elf = 0;
+    for elf in &highest_elves {
+        current_elf += elf;
+    }
+    return current_elf;
+}
---
+++ src/main.rs
@@ -0,0 +1,4 @@
+pub mod day1;
+fn main() {
+    println!("{}", day1::calorie_counter())
+}
