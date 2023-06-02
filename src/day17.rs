a8f24e201d4a991af219dc153531175a46e62633
stefanbalta <stefan.balta.sb@gmail.com>
2023-06-02 17:42:43 -0400

    Working on implementing rock constructor

--- src/day17.rs
+++ src/day17.rs
@@ -18,14 +18,43 @@ impl Rock {
         match rock_type {
             0 => {
                 for i in 0..4 {
-                    new_rock.occupied_locations[i].x = 3 + (i as u16);
+                    new_rock.occupied_locations[i].x = 2 + (i as u16);
                     new_rock.occupied_locations[i].y = current_max + 3;
                 }
             }
-            1 => {}
-            2 => {}
-            3 => {}
-            4 => {}
+            1 => {
+                for i in 0..3 {
+                    new_rock.occupied_locations[i].x = 2 + (i as u16);
+                    new_rock.occupied_locations[i].y = current_max + 4;
+                }
+                new_rock.occupied_locations[3].x = 3;
+                new_rock.occupied_locations[3].y = current_max + 3;
+                new_rock.occupied_locations[4].x = 3;
+                new_rock.occupied_locations[4].y = current_max + 5;
+            }
+            2 => {
+                for i in 0..3 {
+                    new_rock.occupied_locations[i].x = 2 + (i as u16);
+                    new_rock.occupied_locations[i].y = current_max + 3;
+                }
+                new_rock.occupied_locations[3].x = 4;
+                new_rock.occupied_locations[3].y = current_max + 4;
+                new_rock.occupied_locations[4].x = 4;
+                new_rock.occupied_locations[4].y = current_max + 5;
+            }
+            3 => {
+                for i in 0..4 {
+                    new_rock.occupied_locations[i].x = 2;
+                    new_rock.occupied_locations[i].y = current_max + 2 + (i as u16);
+                }}
+            4 => {
+                for i in 0..2{
+                    for j in 0..2{
+                        new_rock.occupied_locations[(i*2)+j].x = 2 + (i as u16);
+                        new_rock.occupied_locations[(i*2)+j].y = current_max + 3 + (j as u16);
+                    }
+                }
+            }
             _ => panic!("Error! Rock Type Not Supported"),
         }
         return new_rock;
@@ -35,5 +64,9 @@ impl Rock {
 struct Cave {}
 pub fn pyroclastic_flow_part_one() -> u16 {
     let input: &str = &std::fs::read_to_string("day17.txt").expect("Error reading from file!");
+    for i in 0..5 {
+        let rock: Rock = Rock::build(i, 0);
+        dbg!(rock);
+    }
     return 0;
 }
